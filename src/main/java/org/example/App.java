30bcf1565a12b449dc99df6ad6cce03c314c2744
antara-walid <Antara.walid.27@gmail.com>
2022-10-10 20:35:24 +0100

    exemple in main class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       // System.out.println( "Hello World!" );
+        HashTable<String> ht = new HashTable<>(3);
+
+        ht.put(1,"walid");
+        ht.put(2,"bora");
+        ht.put(3,"hamza");
+        ht.put(4,"gri3a");
+
+        System.out.println(ht.get(3));
+        System.out.println(ht.get(4));
+
     }
 }
